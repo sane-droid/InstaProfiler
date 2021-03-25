5d314d48be800e42ce0c35cb173f89039cb1c53e
Prajwal Ladkat <62328357+prajwal080501@users.noreply.github.com>
2021-03-25 16:03:55 +0530

    added second activity to display profile information after user enters the instagram username

--- app/src/main/java/com/instaprofiler/app/ui/activities/ProfileActivity.java
+++ app/src/main/java/com/instaprofiler/app/ui/activities/ProfileActivity.java
@@ -5,23 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.instaprofiler.app.R;
 
-public class HomeActivity extends AppCompatActivity {
-    ImageButton home_search_button;
+public class ProfileActivity extends AppCompatActivity {
+    ImageButton profile_back_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        home_search_button = findViewById(R.id.home_search_button);
-        home_search_button.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_profile);
+        profile_back_button = findViewById(R.id.profile_back_button);
+        profile_back_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this, ProfileActivity.class);
+                Intent intent = new Intent(ProfileActivity.this, HomeActivity.class);
                 startActivity(intent);
             }
         });
