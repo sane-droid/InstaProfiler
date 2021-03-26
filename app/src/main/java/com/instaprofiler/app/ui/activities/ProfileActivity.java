08b389b3fec81ee34ad81852cd80d907384b6c11
Sharmaketann <sharmaketann@gmail.com>
2021-03-26 21:11:11 +0530

    logic
    # Conflicts:
    #	.idea/vcs.xml

--- app/src/main/java/com/instaprofiler/app/ui/activities/ProfileActivity.java
+++ app/src/main/java/com/instaprofiler/app/ui/activities/ProfileActivity.java
@@ -1,17 +1,19 @@
 package com.instaprofiler.app.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.instaprofiler.app.R;
 import com.instaprofiler.app.data.model.UserProfile;
+import com.instaprofiler.app.data.repository.ProfilerRepository;
 import com.instaprofiler.app.ui.viewmodel.ProfilerViewModel;
 
 public class ProfileActivity extends AppCompatActivity {
@@ -19,6 +21,14 @@ public class ProfileActivity extends AppCompatActivity {
     ProfilerViewModel profilerViewModel;
     TextView textView,following,followers,name,bio;
     String userId;
+
+    public static class ProfilerViewModel extends ViewModel {
+        public LiveData <UserProfile> liveData = null;
+        ProfilerRepository profilerRepository = new ProfilerRepository();
+        public void getAccountDetail(String userName){
+            liveData = profilerRepository.getAccountDetail(userName);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +42,6 @@ public class ProfileActivity extends AppCompatActivity {
         bio = findViewById(R.id.profileBio);
 
         Intent intent = getIntent();
-        Bundle bundle = intent.getExtras();
         userId = intent.getStringExtra("userName");
 
         ViewModelProvider viewModelProvider = new ViewModelProvider(this,ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()));
@@ -44,7 +53,6 @@ public class ProfileActivity extends AppCompatActivity {
             @Override
             public void onChanged(UserProfile userProfile) {
                 textView.setText(userId);
-
             }
         });
     }
