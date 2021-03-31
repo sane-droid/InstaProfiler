c8a742852ed3823ce11d5afe1249f620a379198f
Sharmaketann <sharmaketann@gmail.com>
2021-03-31 13:49:27 +0530

    drawerLayout
    # Conflicts:
    #	.idea/vcs.xml

--- app/src/main/java/com/instaprofiler/app/ui/activities/HomeActivity.java
+++ app/src/main/java/com/instaprofiler/app/ui/activities/HomeActivity.java
@@ -2,19 +2,27 @@ package com.instaprofiler.app.ui.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
 import com.instaprofiler.app.R;
 
 public class HomeActivity extends AppCompatActivity {
     ImageButton home_search_button, hamburger;
     EditText userName;
-
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +30,21 @@ public class HomeActivity extends AppCompatActivity {
         home_search_button = findViewById(R.id.home_search_button);
         userName = findViewById(R.id.home_insta_username);
         hamburger = findViewById(R.id.hamburger);
+//        drawerLayout = findViewById(R.id.drawerlayout);
+        navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.nav_home:
+                        Toast.makeText(HomeActivity.this, "clicked Home", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                return false;
+            }
+        });
         home_search_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,8 +58,10 @@ public class HomeActivity extends AppCompatActivity {
         hamburger.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this, DrawerLayout.class);
-                startActivity(intent);
+                DrawerLayout navDrawer = findViewById(R.id.drawerlayout);
+                // If the navigation drawer is not open then open it, if its already open then close it.
+                if(!navDrawer.isDrawerOpen(GravityCompat.START)) navDrawer.openDrawer(GravityCompat.START);
+                else navDrawer.closeDrawer(GravityCompat.END);
             }
         });
 
